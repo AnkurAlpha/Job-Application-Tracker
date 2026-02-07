cf6d88a244d6508ee8e75cefdb3220998c50129b
AnkurAlpha <ankuralpha404@gmail.com>
2026-02-07 19:53:46 +0530

    add tailwindcss + fix weird importing of strict mode
    - start working on UI
    - clenaup older css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-      <h1>Job Application Tracker</h1>
-      <Link to="/jobs">Go to Jobs</Link>
+      <h1 className="text-3xl font-bold underline ">Job Application Tracker</h1>
+      <Link className="text-blue-500 transition duration-300 ease-in-out hover:text-blue-600" to="/jobs">Go to Jobs</Link>
     </div>
   );
 }
