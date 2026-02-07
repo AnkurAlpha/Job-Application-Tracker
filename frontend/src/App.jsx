9bdfa00d3e284a0fd1b4db41a0e899f6f96963ef
AnkurAlpha <ankuralpha404@gmail.com>
2026-02-07 20:18:03 +0530

    define a basic Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,19 @@ import Home from "./pages/Home.jsx";
 import Jobs from "./pages/Jobs.jsx";
 import JobDetails from "./pages/JobDetails.jsx";
 import Apply from "./pages/Apply.jsx";
+import Layout from "./components/Layout.jsx";
 
 const App = () => {
-  return (
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/jobs" element={<Jobs />} />
-      <Route path="/jobs/:id" element={<JobDetails />} />
-      <Route path="/apply/:id" element={<Apply />} />
-      </Routes>
-  )
+    return (
+        <Layout>
+            <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/jobs" element={<Jobs />} />
+            <Route path="/jobs/:id" element={<JobDetails />} />
+            <Route path="/apply/:id" element={<Apply />} />
+            </Routes>
+        </Layout>
+    )
 }
 
 export default App;
