ca2732fbe62923d97a1d95e2eaa68c2ac7a5349b
AnkurAlpha <ankuralpha404@gmail.com>
2026-02-08 22:04:36 +0530

    update Homepage + Jobs section
    - Homepage updated
    - Jobs updated
    - Next work on: JobsDetails

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <header className="border-b border-slate-200 bg-white ">
         <div className="mx-auto flex max-w-5xl justify-between px-4"></div>
             <div className="mx-auto flex justify-center text-white bg-black">
-                <p className="text-2xl font-bold"> Job Tracker</p>
+                <p className="text-2xl font-bold"> Job Application Tracker</p>
             </div>
             <nav className="flex gap-2 bg-gradient-to-r
         from-indigo-500 via-purple-500 to-pink-500 py-2 px-2">
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline ">Job Application Tracker</h1>
-      <Link className="text-blue-500 transition duration-300 ease-in-out hover:text-blue-600" to="/jobs">Go to Jobs</Link>
-    </div>
-  );
+    return (
+        <div className="grid gap-6 md:grid-cols-2 transition ease-in-out duration-500">
+            <div className="rounded-2xl border border-slate-200 bg-white p-8 shadow-sm
+      hover:shadow-purple-500 transform duration-500 hover:scale-105">
+                <h1 className="text-3xl font-bold tracking-tight">Job Application Tracker</h1>
+                <p className="mt-3 text-slate-600">
+                    Track jobs and quickly submit applications
+                </p>
+                <div className="mt-6 flex gap-3">
+                    <Link to="/dashboard" className="rounded-xl bg-slate-900 px-4 py-2 text-sm
+                  font-semibold text-white hover:bg-gradient-to-r
+                  hover:from-indigo-70 hover:via-purple-700 hover:to-pink-700
+                  transition duration-500 ease-in-out"> Dashboard</Link>
+                    <Link to="/jobs" className="rounded-xl bg-slate-900 px-4 py-2 text-sm
+                  font-semibold text-white hover:bg-gradient-to-r
+                  hover:from-indigo-70 hover:via-purple-700 hover:to-pink-700
+                  transition duration-500 ease-in-out"> Jobs</Link>
+                </div>
+            </div>
+        </div>
+    );
 }
 
